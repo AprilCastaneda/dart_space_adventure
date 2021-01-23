229d4fe129a6b18effeec6d1a5cb5e5c6c9de856
April Castaneda <castanap@oregonstate.edu>
2021-01-22 23:46:38 -0800

    First commit

---
+++ lib/src/planetary_system.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+import 'planet.dart';
+
+class PlanetarySystem {
+
+  final Random _random = Random();
+  final String name;
+  final List<Planet> planets;
+  final height = 1;
+  final width = 2;
+  
+  PlanetarySystem({this.name = 'Unnamed System', this.planets = const[]});
+  
+  int get numberOfPlanets => planets.length;
+  bool get hasPlanets => planets.isNotEmpty;
+  int get thisVolume {
+    this.height * this.width;
+  }
+  
+  Planet randomPlanet() {
+    if (!hasPlanets) return Planet.nullPlanet();
+    return planets[_random.nextInt(planets.length)];
+  }
+
+  Planet planetWithName(String name){
+    return planets.firstWhere( 
+      (planet) => planet.name == name,
+      orElse: () => Planet.nullPlanet()
+    );
+  }
+}
